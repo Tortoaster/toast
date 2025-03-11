37a887b5e0e718ce08ce285e23258d4b14d57603
Rick van der Wal <rick@tortoaster.com>
2025-03-11 15:58:55 +0100

    WIP: Replace client

--- frontend/src/routes/projects/[id]/+page.ts
+++ frontend/src/routes/projects/[id]/+page.ts
@@ -1,17 +1,22 @@
 import type { PageLoad } from "./$types";
 import { marked } from "marked";
 import { PUBLIC_API_URL, PUBLIC_BUCKET_URL } from "$env/static/public";
-import { Configuration, DefaultApi } from "$lib/client";
+import type { paths } from "$lib/client";
+import createClient from "openapi-fetch";
 
 export const load: PageLoad = async ({ params, fetch }) => {
-  const api = new DefaultApi(new Configuration({ basePath: PUBLIC_API_URL }));
-  const project = await api.getProject({ id: params.id });
+  const client = createClient<paths>({ baseUrl: PUBLIC_API_URL, fetch: fetch });
+
+  const project = await client.GET("/projects/{id}", {
+    params: { path: { id: params.id } },
+  }).then(({ data }) => data!);
+  const comments = client.GET("/projects/{project_id}/comments", {
+    params: { path: { project_id: params.id } },
+  }).then(({ data }) => data!);
 
   const content = await fetch(`${PUBLIC_BUCKET_URL}/content/${project.id}.md`)
     .then((response) => response.text())
     .then((md) => marked.parse(md));
-
-  const comments = api.listComments({ projectId: params.id });
 
   return { project, content, comments };
 };
