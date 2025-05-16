7b26339f9d5c9d69c16bb6c38f4b2816a1c81203
Rick van der Wal <rick@tortoaster.com>
2025-05-16 21:49:52 +0200

    Remove unused errors

--- backend/src/error.rs
+++ backend/src/error.rs
@@ -1,8 +1,5 @@
 use axum::{
-    extract::{
-        multipart::MultipartRejection,
-        rejection::{FormRejection, QueryRejection},
-    },
+    extract::rejection::QueryRejection,
     http::StatusCode,
     response::{IntoResponse, Response},
 };
@@ -17,16 +14,8 @@ pub type WithAppRejection<E> = WithRejection<E, AppError>;
 pub enum AppError {
     #[error("I couldn't access the database! :(")]
     Database(#[from] sqlx::Error),
-    #[error("Something weird went wrong :(")]
-    MultipartError(#[from] axum::extract::multipart::MultipartError),
     #[error("I couldn't find the page you're looking for! :(")]
     NotFound,
-    #[error("Please fill out all the fields!")]
-    Form(#[from] FormRejection),
-    #[error("Something weird went wrong :(")]
-    MultipartRejection(#[from] MultipartRejection),
-    #[error("Please change the following fields :3\n{0}")]
-    ValidateForm(#[from] axum_valid::ValidRejection<FormRejection>),
     #[error("Please change the following fields :3\n{0}")]
     ValidateQuery(#[from] axum_valid::ValidRejection<QueryRejection>),
 }
@@ -36,11 +25,7 @@ impl AppError {
         match self {
             AppError::Database(_) => StatusCode::SERVICE_UNAVAILABLE,
             AppError::NotFound => StatusCode::NOT_FOUND,
-            AppError::Form(_)
-            | AppError::MultipartError(_)
-            | AppError::MultipartRejection(_)
-            | AppError::ValidateForm(_)
-            | AppError::ValidateQuery(_) => StatusCode::BAD_REQUEST,
+            AppError::ValidateQuery(_) => StatusCode::BAD_REQUEST,
         }
     }
 }
